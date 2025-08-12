afca932a8888e765ec6e26b6c799f0ccf587d9cf
Ujjwal <ujjwalsharma.ece@gmail.com>
2025-08-12 16:24:57 +0530

    DayNine finished

--- src/main/java/com/online/orderapp/exception/PaymentFailedException.java
+++ src/main/java/com/online/orderapp/exception/PaymentFailedException.java
@@ -2,6 +2,9 @@ package com.online.orderapp.exception;
 
 public class PaymentFailedException extends RuntimeException {
 	
+
+	private static final long serialVersionUID = 1L;
+
 	public PaymentFailedException(String message) {
 		super(message);
 	}
