76eaeeedff6afacde7bab900fdf27e432005a0cd
Ujjwal <ujjwalsharma.ece@gmail.com>
2025-08-14 16:41:38 +0530

    User controller completed

--- src/main/java/com/online/orderapp/controller/UserController.java
+++ src/main/java/com/online/orderapp/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,6 +62,23 @@ public class UserController {
 		apiResponse.setMessage("Data Fetched Acoording to page");
 		apiResponse.setStatusCode(HttpStatus.OK.value());		
 		return ResponseEntity.ok(apiResponse);				
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<ResponseStructure<User>> updateUser(@RequestParam int id, @RequestBody User user){
+		User response = userService.updateUser(user, id);
+		ResponseStructure<User> apiResponse = new ResponseStructure<>();
+		apiResponse.setData(response);
+		apiResponse.setMessage("User updated successfully");
+		apiResponse.setStatusCode(HttpStatus.OK.value());
+		
+		return ResponseEntity.ok(apiResponse);
+	}
+	
+	@DeleteMapping("/{id}/delete")
+	public ResponseEntity<User> deleteUser(@PathVariable Integer id){
+		userService.deleteUser(id);
+		return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
 	}
 	
 	@PatchMapping("/{id}/user/uploadImage")
