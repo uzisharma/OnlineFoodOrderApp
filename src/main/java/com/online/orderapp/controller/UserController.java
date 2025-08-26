fd13698873ccb6162cc635178b5a30b15c6410dc
Ujjwal <ujjwalsharma.ece@gmail.com>
2025-08-26 19:25:47 +0530

    Response and Request dto for userlogin

--- src/main/java/com/online/orderapp/controller/UserController.java
+++ src/main/java/com/online/orderapp/controller/UserController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.online.orderapp.dto.ResponseStructure;
+import com.online.orderapp.dto.UserLoginRequestDto;
 import com.online.orderapp.dto.UserLoginResponseDto;
 import com.online.orderapp.entity.User;
 import com.online.orderapp.service.UserService;
@@ -114,8 +115,13 @@ public class UserController {
 	
 	
 	@PostMapping("/login")
-	public ResponseEntity<ResponseStructure<UserLoginResponseDto>> login(@RequestBody User loginRequest){
-		UserLoginResponseDto response = userService.login(loginRequest.getUserName(), loginRequest.getPassword());
+	public ResponseEntity<ResponseStructure<UserLoginResponseDto>> login(
+			@RequestBody UserLoginRequestDto loginRequest){
+		
+		UserLoginResponseDto response = userService.login(
+				loginRequest.getUserName(), 
+				loginRequest.getPassword()
+				);
 		ResponseStructure<UserLoginResponseDto> apiResponse = new ResponseStructure<>();
 		apiResponse.setData(response);
 		apiResponse.setMessage("Login Success");
--- src/main/java/com/online/orderapp/entity/User.java
+++ src/main/java/com/online/orderapp/entity/User.java
@@ -3,6 +3,9 @@ package com.online.orderapp.entity;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -30,7 +33,7 @@ public class User {
 	@NotBlank
 	private String userName;
 	
-	@JsonIgnore
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@NotNull
 	@NotBlank
 	private String password;
